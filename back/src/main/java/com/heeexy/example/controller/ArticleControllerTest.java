e1012204f126364ce64bdaf1a666ea8a0c42dafd
ldydxmj <mingjin009@126.com>
2020-01-04 10:59:55 +0800

    修改配置适应nginx

---
+++ back/src/main/java/com/heeexy/example/controller/ArticleControllerTest.java
@@ -0,0 +1,23 @@
+package com.heeexy.example.controller;
+
+
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class ArticleControllerTest {
+    private static final String openid = "abc";
+
+
+    @Autowired
+   @Test
+    public void findSellerInfoByOpenid() throws Exception {
+System.out.println("3333333aaa");
+System.out.println("3333333aaa");
+System.out.println("3333333aaa");System.out.println("3333333aaa");
+System.out.println("3333333aaa");
+System.out.println("3333333aaa");
+System.out.println("3333333aaa");
+System.out.println("3333333aaa");
+        System.out.println(openid);
+    }}
